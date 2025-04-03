6f99962a035677f57c8cd92675efced5a8bafd21
max-jeronimo <maxjeronimo05@gmail.com>
2025-04-03 19:20:43 -0400

    set template for hobbys

--- src/components/hobby1.tsx
+++ src/components/hobby1.tsx
@@ -1,4 +1,6 @@
 import Table1 from "./table1";
+import List1 from "./list1";
+import Form1 from "./form1";
 
 const Hobby1 = () => {
 
@@ -7,7 +9,9 @@ const Hobby1 = () => {
         <p>Hello my name is Max Jeronimo!</p>
         <p>I'm a sophomore and I major in computer science.</p>
         <p>I'm currently taking Software Engineering, Foundations of Computer Science, and Introduction to Material Science.</p>
-           <Table1/>
+           <Table1 />
+           <List1 />
+           <Form1 />
        </div>
 
     )
--- src/components/hobby2.tsx
+++ src/components/hobby2.tsx
@@ -1,7 +1,19 @@
+import Table2 from "./table1";
+import List2 from "./list1";
+import Form2 from "./form1";
+
 const Hobby2 = () => {
 
     return (
-        <h1>CS3733-D25 Team 7: Soccer and Lifting</h1>
+        <div>
+            <p>Hello my name is Max Jeronimo!</p>
+            <p>I'm a sophomore and I major in computer science.</p>
+            <p>I'm currently taking Software Engineering, Foundations of Computer Science, and Introduction to Material Science.</p>
+            <Table2 />
+            <List2 />
+            <Form2 />
+        </div>
+
     )
 }
 
